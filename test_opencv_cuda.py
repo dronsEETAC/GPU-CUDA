ca0656433ffd6c8dd8d390bd4799922c89a5644b
dronsEETAC <100842082+dronsEETAC@users.noreply.github.com>
2026-08-04 12:09:18 +0200

    Update test_opencv_cuda.py

--- test_opencv_cuda.py
+++ test_opencv_cuda.py
@@ -1,30 +1,83 @@
-#
+import os
+os.add_dll_directory(r"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v12.9\bin")
+
 import cv2
+import time
 
-info = cv2.getBuildInformation()
+# ==========================================================
+# Cargar imagen
+# ==========================================================
 
-if "CUDA" in info:
+imagen = cv2.imread("imagen.png")
 
-    # Leer una imagen (CPU)
-    imagen = cv2.imread("imagen.jpg")
+if imagen is None:
+    raise Exception("No se pudo abrir la imagen")
 
-    # Crear un objeto GpuMat
-    gpu = cv2.cuda_GpuMat()
+print("Resolución:", imagen.shape)
 
-    # Copiar la imagen a la GPU
-    gpu.upload(imagen)
+# ==========================================================
+# CPU
+# ==========================================================
 
-    # Convertir a escala de grises en la GPU
-    gris_gpu = cv2.cuda.cvtColor(gpu, cv2.COLOR_BGR2GRAY)
+inicio = time.perf_counter()
 
-    # Recuperar la imagen
-    gris = gris_gpu.download()
+gris = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
 
-    cv2.imshow("Original", imagen)
-    cv2.imshow("Gris", gris)
+bordes_cpu = cv2.Canny(
+    gris,
+    100,
+    200
+)
 
-    cv2.waitKey(0)
-else:
-    print("Parece que esta versión de OpenCV no tiene soporte para CUDA.")
+fin = time.perf_counter()
+
+print(f"CPU : {(fin-inicio)*1000:.1f} ms")
+
+# ==========================================================
+# GPU
+# ==========================================================
+
+gpu = cv2.cuda_GpuMat()
+
+gpu.upload(imagen)
+
+# Conversión a gris
+gpu_gris = cv2.cuda.cvtColor(gpu, cv2.COLOR_BGR2GRAY)
+
+# Crear detector Canny
+canny = cv2.cuda.createCannyEdgeDetector(
+    100,
+    200
+)
+
+# ----------------------------
+# Calentamiento
+# ----------------------------
+
+_ = canny.detect(gpu_gris)
+_.download()
+
+# ----------------------------
+# Medición
+# ----------------------------
+
+inicio = time.perf_counter()
+
+gpu_bordes = canny.detect(gpu_gris)
+
+bordes_gpu = gpu_bordes.download()
+
+fin = time.perf_counter()
+
+print(f"GPU : {(fin-inicio)*1000:.1f} ms")
+
+
+# ==========================================================
+# Guardar resultados
+# ==========================================================
+
+cv2.imwrite("bordes_cpu2.jpg", bordes_cpu)
+cv2.imwrite("bordes_gpu2.jpg", bordes_gpu)
+
 
 
